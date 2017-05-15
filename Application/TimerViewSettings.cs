1955fb66a58003cc330ce742c8edbc7e754af6bc
Fikayo Odunayo <odunayofikayo@gmail.com>
2017-05-15 19:36:31 -0400

    Added new class for visual timer settings

--- Application/TimerViewSettings.cs
+++ Application/TimerViewSettings.cs
@@ -37,43 +37,145 @@
             }
         }
 
-        #region Display Settings
+        public TimerVisualSettings VisualSettings { get; set; }
 
-        public double Duration { get; set; } // In seconds
+        #region Visual Settings
 
-        public Font TimerFont { get; set; }
+        public Font TimerFont
+        {
+            get { return this.VisualSettings.TimerFont; }
 
-        public TimerCounterMode CounterMode { get; set; }
+            set
+            {
+                this.VisualSettings.TimerFont = value;
+            }
+        }
 
-        public TimerDisplayMode DisplayMode { get; set; }
+        public TimerCounterMode CounterMode
+        {
+            get { return this.VisualSettings.CounterMode; }
 
-        public string FinalMessage { get; set; }
+            set
+            {
+                this.VisualSettings.CounterMode = value;
+            }
+        }
 
-        #endregion
+        public TimerDisplayMode DisplayMode
+        {
+            get { return this.VisualSettings.DisplayMode; }
 
-        #region Colors
+            set
+            {
+                this.VisualSettings.DisplayMode = value;
+            }
+        }
 
-        public Color TimerColor { get; set; }
+        public string FinalMessage
+        {
+            get { return this.VisualSettings.FinalMessage; }
 
-        public Color RunningColor { get; set; }
+            set
+            {
+                this.VisualSettings.FinalMessage = value;
+            }
+        }
 
-        public Color PausedColor { get; set; }
+        public Color TimerColor
+        {
+            get { return this.VisualSettings.TimerColor; }
 
-        public Color WarningColor { get; set; }
+            set
+            {
+                this.VisualSettings.TimerColor = value;
+            }
+        }
 
-		public Color SecondWarningColor { get; set; }
+        public Color RunningColor
+        {
+            get { return this.VisualSettings.RunningColor; }
 
-        public Color StoppedColor { get; set; }
+            set
+            {
+                this.VisualSettings.RunningColor = value;
+            }
+        }
 
-        public Color ExpiredColor { get; set; }
+        public Color PausedColor
+        {
+            get { return this.VisualSettings.PausedColor; }
 
-        public Color BackgroundColor { get; set; }
+            set
+            {
+                this.VisualSettings.PausedColor = value;
+            }
+        }
 
-        public Color MessageColor { get; set; }
+        public Color WarningColor
+        {
+            get { return this.VisualSettings.WarningColor; }
+
+            set
+            {
+                this.VisualSettings.WarningColor = value;
+            }
+        }
+
+        public Color SecondWarningColor
+        {
+            get { return this.VisualSettings.SecondWarningColor; }
+
+            set
+            {
+                this.VisualSettings.SecondWarningColor = value;
+            }
+        }
+
+        public Color StoppedColor
+        {
+            get { return this.VisualSettings.StoppedColor; }
+
+            set
+            {
+                this.VisualSettings.StoppedColor = value;
+            }
+        }
+
+        public Color ExpiredColor
+        {
+            get { return this.VisualSettings.ExpiredColor; }
+
+            set
+            {
+                this.VisualSettings.ExpiredColor = value;
+            }
+        }
+
+        public Color BackgroundColor
+        {
+            get { return this.VisualSettings.BackgroundColor; }
+
+            set
+            {
+                this.VisualSettings.BackgroundColor = value;
+            }
+        }
+
+        public Color MessageColor
+        {
+            get { return this.VisualSettings.MessageColor; }
+
+            set
+            {
+                this.VisualSettings.MessageColor = value;
+            }
+        }
 
         #endregion
 
         #region Times
+
+        public double Duration { get; set; } // In seconds
 
         public double WarningTime { get; set; }
 
@@ -81,54 +183,32 @@
 
         #endregion
 
-		public bool BlinkOnExpired { get; set; }
+        public bool BlinkOnExpired { get; set; }
 
-		public bool HasFirstWarning
-		{
-			get { return this.WarningTime > 0; }
-		}
+        public bool HasFirstWarning
+        {
+            get { return this.WarningTime > 0; }
+        }
 
-		public bool HasSecondWarning
-		{
-			get { return this.SecondWarningTime > 0; }
-		}
-
+        public bool HasSecondWarning
+        {
+            get { return this.SecondWarningTime > 0; }
+        }
+        
         public void SetFont(string fontFamily = "", float fontSize = 0f)
         {
-            fontSize = fontSize <= 0 ? this.TimerFont.Size : fontSize;
-
-            if (string.IsNullOrEmpty(fontFamily))
-            {
-                this.TimerFont = new Font(this.TimerFont.FontFamily, fontSize, FontStyle.Regular, GraphicsUnit.Point, 0);
-            }
-            else
-            {
-                this.TimerFont = new Font(fontFamily, fontSize, FontStyle.Regular, GraphicsUnit.Point, 0);
-            }
+            this.VisualSettings.SetFont(fontFamily, fontSize);
         }
 
         public string SaveSettingsAsCsv()
         {
-			return string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18}",
+            return string.Format("{0},{1},{2},{3},{4},{5}",
                 this.Name,
                 this.Duration,
-                this.TimerFont.FontFamily.Name,
-                this.TimerFont.Size,
-                this.CounterMode,
-                this.DisplayMode,
-                this.FinalMessage,
-                this.TimerColor.Name,
-                this.RunningColor.Name,
-                this.PausedColor.Name,
-                this.WarningColor.Name,
-                this.StoppedColor.Name,
-                this.ExpiredColor.Name,
-                this.BackgroundColor.Name,
-                this.MessageColor.Name,
                 this.WarningTime,
                 this.SecondWarningTime,
-			    this.SecondWarningColor,
-			    this.BlinkOnExpired);
+                this.BlinkOnExpired,
+                this.VisualSettings.SaveSettingsAsCsv());
         }
 
         // override object.Equals
@@ -144,23 +224,10 @@
 
             return this.Name.Equals(that.Name)
                 && this.Duration.Equals(that.Duration)
-                && this.TimerFont.FontFamily.Name.Equals(that.TimerFont.FontFamily.Name)
-                && this.TimerFont.Size.Equals(that.TimerFont.Size)
-                && this.CounterMode.Equals(that.CounterMode)
-                && this.DisplayMode.Equals(that.DisplayMode)
-                && this.FinalMessage.Equals(that.FinalMessage)
-                && this.TimerColor.Name.Equals(that.TimerColor)
-                && this.RunningColor.Equals(that.RunningColor)
-                && this.PausedColor.Equals(that.PausedColor)
-                && this.WarningColor.Equals(that.WarningColor)
-                && this.StoppedColor.Equals(that.StoppedColor)
-                && this.ExpiredColor.Equals(that.ExpiredColor)
-                && this.BackgroundColor.Equals(that.BackgroundColor)
-                && this.MessageColor.Equals(that.MessageColor)
                 && this.WarningTime.Equals(that.WarningTime)
                 && this.SecondWarningTime.Equals(that.SecondWarningTime)
-                && this.SecondWarningColor.Equals(that.SecondWarningColor)
-                && this.BlinkOnExpired.Equals(that.BlinkOnExpired);
+                && this.BlinkOnExpired.Equals(that.BlinkOnExpired)
+                && this.VisualSettings.Equals(that.VisualSettings);
         }
 
         // override object.GetHashCode
@@ -178,30 +245,15 @@
                 var values = csv.Split(new char[] { ',' });
                 settings.Name = values[0];
                 settings.Duration = double.Parse(values[1]);
+                settings.WarningTime = double.Parse(values[2]);
+                settings.SecondWarningTime = double.Parse(values[3]);
+                settings.BlinkOnExpired = bool.Parse(values[4]);
 
-                var fontFamily = values[2];
-                var fontSize = float.Parse(values[3]);
-                settings.SetFont(fontFamily, fontSize);
-
-                settings.CounterMode = Util.ToEnum<TimerCounterMode>(values[4]);
-                settings.DisplayMode = Util.ToEnum<TimerDisplayMode>(values[5]);
-                settings.FinalMessage = values[6];
-                settings.TimerColor = Util.FromARGBString(Color.FromName(values[7]));
-                settings.RunningColor = Util.FromARGBString(Color.FromName(values[8]));
-                settings.PausedColor = Util.FromARGBString(Color.FromName(values[9]));
-                settings.WarningColor = Util.FromARGBString(Color.FromName(values[10]));
-                settings.StoppedColor = Util.FromARGBString(Color.FromName(values[11]));
-                settings.ExpiredColor = Util.FromARGBString(Color.FromName(values[12]));
-                settings.BackgroundColor = Util.FromARGBString(Color.FromName(values[13]));
-                settings.MessageColor = Util.FromARGBString(Color.FromName(values[14]));
-                settings.WarningTime = double.Parse(values[15]);
-                settings.SecondWarningTime = double.Parse(values[16]);
-				settings.SecondWarningColor = Util.FromARGBString(Color.FromName(values[17]));
-				settings.BlinkOnExpired = bool.Parse(values[18]);
+                settings.VisualSettings = TimerVisualSettings.ParseCsv(csv, 5);
 
                 return settings;
             }
-			catch
+            catch
             {
                 return settings;
             }
@@ -209,21 +261,22 @@
 
         private void SetDefaultSettings()
         {
-            this.CounterMode = TimerCounterMode.CountDownToMinus;
-            this.DisplayMode = TimerDisplayMode.FullWidth;
-            this.SetFont("Arial", 250f);
+            //this.CounterMode = TimerCounterMode.CountDownToMinus;
+            //this.DisplayMode = TimerDisplayMode.FullWidth;
+            //this.SetFont("Arial", 250f);
 
-            this.TimerColor = Color.White;
-            this.RunningColor = Color.White;
-            this.PausedColor = Color.Cyan;
-            this.WarningColor = Color.Yellow;
-			this.SecondWarningColor = Color.Orange;
-            this.ExpiredColor = Color.Red;
-            this.StoppedColor = Color.Silver;
-            this.BackgroundColor = Color.Black;
-            this.MessageColor = Color.DodgerBlue;
+            //this.TimerColor = Color.White;
+            //this.RunningColor = Color.White;
+            //this.PausedColor = Color.Cyan;
+            //this.WarningColor = Color.Yellow;
+            //this.SecondWarningColor = Color.Orange;
+            //this.ExpiredColor = Color.Red;
+            //this.StoppedColor = Color.Silver;
+            //this.BackgroundColor = Color.Black;
+            //this.MessageColor = Color.DodgerBlue;
 
-			this.BlinkOnExpired = false;
+            this.VisualSettings = TimerVisualSettings.Default;
+            this.BlinkOnExpired = false;
         }
 
         public static TimerViewSettings Default
@@ -233,12 +286,178 @@
 
         public enum TimerCounterMode
         {
-			CountDownToMinus, CountDownToZero, CountUp
+            CountDownToMinus, CountDownToZero, CountUp
         }
 
         public enum TimerDisplayMode
         {
             FullWidth, SuppressLeadingZeros, DisplayInSeconds
+        }
+
+        public class TimerVisualSettings
+        {
+
+            public TimerVisualSettings()
+            {
+                this.SetDefaultSettings();
+                TimerViewSettings.count++;
+            }
+
+            #region Display Settings
+
+            public Font TimerFont { get; set; }
+
+            public TimerCounterMode CounterMode { get; set; }
+
+            public TimerDisplayMode DisplayMode { get; set; }
+
+            public string FinalMessage { get; set; }
+
+            #endregion
+
+            #region Colors
+
+            public Color TimerColor { get; set; }
+
+            public Color RunningColor { get; set; }
+
+            public Color PausedColor { get; set; }
+
+            public Color WarningColor { get; set; }
+
+            public Color SecondWarningColor { get; set; }
+
+            public Color StoppedColor { get; set; }
+
+            public Color ExpiredColor { get; set; }
+
+            public Color BackgroundColor { get; set; }
+
+            public Color MessageColor { get; set; }
+
+            #endregion
+
+            public static TimerVisualSettings Default
+            {
+                get { return new TimerVisualSettings(); }
+            }
+
+            public void SetFont(string fontFamily = "", float fontSize = 0f)
+            {
+                fontSize = fontSize <= 0 ? this.TimerFont.Size : fontSize;
+
+                if (string.IsNullOrEmpty(fontFamily))
+                {
+                    this.TimerFont = new Font(this.TimerFont.FontFamily, fontSize, FontStyle.Regular, GraphicsUnit.Point, 0);
+                }
+                else
+                {
+                    this.TimerFont = new Font(fontFamily, fontSize, FontStyle.Regular, GraphicsUnit.Point, 0);
+                }
+            }
+
+            public string SaveSettingsAsCsv()
+            {
+                return string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13}",
+                    this.TimerFont.FontFamily.Name,
+                    this.TimerFont.Size,
+                    this.CounterMode,
+                    this.DisplayMode,
+                    this.FinalMessage,
+                    this.TimerColor.Name,
+                    this.RunningColor.Name,
+                    this.PausedColor.Name,
+                    this.WarningColor.Name,
+                    this.StoppedColor.Name,
+                    this.ExpiredColor.Name,
+                    this.BackgroundColor.Name,
+                    this.MessageColor.Name,
+                    this.SecondWarningColor);
+            }
+
+            // override object.Equals
+            public override bool Equals(object obj)
+            {
+                if (obj == null || GetType() != obj.GetType())
+                {
+                    return false;
+                }
+
+                TimerVisualSettings that = obj as TimerVisualSettings;
+                if (that == null) return false;
+
+                return this.TimerFont.FontFamily.Name.Equals(that.TimerFont.FontFamily.Name)
+                    && this.TimerFont.Size.Equals(that.TimerFont.Size)
+                    && this.CounterMode.Equals(that.CounterMode)
+                    && this.DisplayMode.Equals(that.DisplayMode)
+                    && this.FinalMessage.Equals(that.FinalMessage)
+                    && this.TimerColor.Name.Equals(that.TimerColor)
+                    && this.RunningColor.Equals(that.RunningColor)
+                    && this.PausedColor.Equals(that.PausedColor)
+                    && this.WarningColor.Equals(that.WarningColor)
+                    && this.StoppedColor.Equals(that.StoppedColor)
+                    && this.ExpiredColor.Equals(that.ExpiredColor)
+                    && this.BackgroundColor.Equals(that.BackgroundColor)
+                    && this.MessageColor.Equals(that.MessageColor)
+                    && this.SecondWarningColor.Equals(that.SecondWarningColor);
+            }
+
+            // override object.GetHashCode
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+
+            public static TimerVisualSettings ParseCsv(string csv, int start = 0)
+            {
+                TimerVisualSettings settings = TimerVisualSettings.Default;
+
+                try
+                {
+                    var values = csv.Split(new char[] { ',' });
+
+                    var fontFamily = values[start + 0];
+                    var fontSize = float.Parse(values[start + 1]);
+                    settings.SetFont(fontFamily, fontSize);
+
+                    settings.CounterMode = Util.ToEnum<TimerCounterMode>(values[start + 2]);
+                    settings.DisplayMode = Util.ToEnum<TimerDisplayMode>(values[start + 3]);
+                    settings.FinalMessage = values[4];
+                    settings.TimerColor = Util.FromARGBString(Color.FromName(values[start + 5]));
+                    settings.RunningColor = Util.FromARGBString(Color.FromName(values[start + 6]));
+                    settings.PausedColor = Util.FromARGBString(Color.FromName(values[start + 7]));
+                    settings.WarningColor = Util.FromARGBString(Color.FromName(values[start + 8]));
+                    settings.StoppedColor = Util.FromARGBString(Color.FromName(values[start + 9]));
+                    settings.ExpiredColor = Util.FromARGBString(Color.FromName(values[start + 10]));
+                    settings.BackgroundColor = Util.FromARGBString(Color.FromName(values[start + 11]));
+                    settings.MessageColor = Util.FromARGBString(Color.FromName(values[start + 12]));
+                    settings.SecondWarningColor = Util.FromARGBString(Color.FromName(values[start + 13]));
+
+                    return settings;
+                }
+                catch
+                {
+                    return settings;
+                }
+            }
+
+            private void SetDefaultSettings()
+            {
+                this.CounterMode = TimerCounterMode.CountDownToMinus;
+                this.DisplayMode = TimerDisplayMode.FullWidth;
+                this.SetFont("Arial", 250f);
+
+                this.TimerColor = Color.White;
+                this.RunningColor = Color.White;
+                this.PausedColor = Color.Cyan;
+                this.WarningColor = Color.Yellow;
+                this.SecondWarningColor = Color.Orange;
+                this.ExpiredColor = Color.Red;
+                this.StoppedColor = Color.Silver;
+                this.BackgroundColor = Color.Black;
+                this.MessageColor = Color.DodgerBlue;
+            }
+
         }
     }
 }
