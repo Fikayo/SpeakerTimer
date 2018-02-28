60334008557b57e1f67b19ef5e3134989dcb9cf7
Fikayo Odunayo <fikayo.odunayo@telus.com>
2018-02-27 20:05:03 -0500

    Added ApplicationLibraries which contains TheLiveTimer.Network

---
+++ ApplicationLibaries/TheLiveTimer.Network/TheLiveTimer.Network/NetworkUtils.cs
@@ -0,0 +1,30 @@
+﻿namespace TheLiveTimer.Network
+{
+    using System;
+    using System.IO;
+    using System.Runtime.Serialization.Formatters.Binary;
+    using System.Text;
+
+    public static class NetworkUtils
+    {
+        public static string GetString(byte[] bytes)
+        {
+            return Encoding.ASCII.GetString(bytes);
+        }
+
+        public static byte[] ObjectToByteArray(string str)
+        {
+            return Encoding.ASCII.GetBytes(str);
+        }
+
+        public static byte[] ObjectToByteArray(Object obj)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            using (var ms = new MemoryStream())
+            {
+                bf.Serialize(ms, obj);
+                return ms.ToArray();
+            }
+        }
+    }
+}
